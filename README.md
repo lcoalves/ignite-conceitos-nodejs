8966452fc437be26c3a4ac3819c66f012279cb8e
Lucas Alves <lucas.alves@afferolab.com.br>
2021-03-12 15:52:33 -0300

    feat: Finish first challenge

---
+++ README.md
@@ -0,0 +1,87 @@
+<h1 align="center">
+  <img alt="Desafio 01 Ignite Conceitos NodeJS" title="Desafio 01 Ignite Conceitos NodeJS" src=".github/gostack-node.png" width="200px" />
+</h1>
+
+<h3 align="center">
+  Desafio 01 Ignite Rocketseat: Conceitos NodeJS.
+  <br>
+  Acima foi construido uma API REST que cria, lista, atualiza e remove (CRUD) 'to.dos'.
+</h3>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/lcoalves/ignite-conceitos-nodejs?color=%2304D361">
+
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-%2304D361">
+
+  <a href="https://github.com/lcoalves">
+    <img alt="Made by Lucas Alves" src="https://img.shields.io/badge/made%20by-Lucas%20Alves-%2304D361">
+  </a>
+
+  <a href="https://github.com/lcoalves/ignite-conceitos-nodejs/stargazers">
+    <img alt="Stargazers" src="https://img.shields.io/github/stars/lcoalves/ignite-conceitos-nodejs?style=social">
+  </a>
+</p>
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+- [NodeJS](https://nodejs.org/en/) - Environment runtime
+- [Yarn](https://yarnpkg.com/getting-started/install) - Packager manager
+
+What things you need to install the software and how to install them
+
+https
+```
+git clone https://github.com/lcoalves/ignite-conceitos-nodejs.git
+```
+ssh
+```
+git clone git@github.com:lcoalves/ignite-conceitos-nodejs.git
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+#### Databases
+First install dependencies
+```
+cd ignite-conceitos-nodejs && yarn
+```
+Next open the code
+```
+code .
+```
+
+#### Back-end
+Start back-end service
+```
+yarn dev
+```
+
+#### Back-end (Running TESTS)
+Run
+```
+yarn test
+```
+
+## Built With
+
+* [Express](https://expressjs.com/pt-br/starter/installing.html) - Fast, unopinionated, minimalist web framework for Node.js
+* [Jest](https://jestjs.io/docs/en/getting-started) - Jest is a delightful JavaScript Testing Framework with a focus on simplicity.
+
+## Authors
+
+* **Lucas Alves** - *IT Development Specialist* - [GitHub profile](https://github.com/lcoalves)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/lcoalves/ignite-conceitos-nodejs/blob/master/LICENSE) file for details
+
+## Acknowledgments
+
+* Node
+* Express
+* Jest
--- src/index.js
+++ src/index.js
@@ -1,41 +1,119 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).send({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+function checksExistsUserTodo(request, response, next) {
+  const { id } = request.params;
+
+  const { user } = request;
+
+  const todos = user.todos;
+
+  const todo = todos.find(todo => id === todo.id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  request.todo = todo;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const userFinded = users.find((user) => user.username === username);
+
+  if (userFinded) {
+    return response.status(400).json({ error: "User already exists" });
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+
+  const { user } = request;
+
+  const id = uuidv4();
+
+  const todo = {
+    id,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.put('/todos/:id', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { title, deadline } = request.body;
+
+  const { todo } = request;
+
+  todo.title = title;
+  todo.deadline = deadline;
+
+  return response.status(201).json(todo);
 });
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { todo } = request;
+
+  todo.done = true
+
+  return response.status(201).json(todo);
 });
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.delete('/todos/:id', checksExistsUserAccount, checksExistsUserTodo, (request, response) => {
+  const { user, todo } = request;
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
